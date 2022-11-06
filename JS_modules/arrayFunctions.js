3a35129bb52b0af54b66b307d319e65854a26163
Javi <monmany1992@gmail.com>
2022-11-06 18:28:04 +0100

    Addedsonar-project

--- JS_modules/arrayFunctions.js
+++ JS_modules/arrayFunctions.js
@@ -1,7 +1,10 @@
 export function arrayLength(arr) {
     let i = 0;
 
-    while (arr[i] !== undefined) {
+    while (
+        arr[i] !== undefined ||
+        (arr[i] === undefined && arr[i + 1] !== undefined)
+    ) {
         i++;
     }
     return i;
--- JS_modules/arrayFunctions.test.js
+++ JS_modules/arrayFunctions.test.js
@@ -1,11 +1,38 @@
-import arrayLength from './functionLength.js';
+// %p - pretty-format.
+// %s- String.
+// %d- Number.
+// %i - Integer.
+// %f - Floating point value.
+// %j - JSON.
+// %o - Object.
+// %# - Index of the test case.
+// %% - single percent sign ('%'). This does not consume an argument
 
-const cases = [5, 'Hello', false, NaN, null, 38, true, undefined];
+import {arrayLength} from './arrayFunctions';
+
+const cases = [5, 'Hello', false, NaN, null, 38, true];
+const extraCase = 50
 
 describe('Given arrayLength function', () => {
-    test(`The length of the given array is %i`, () => {
+    test('The length of the given array should be 7', () => {
         const expected = cases.length;
         const result = arrayLength(cases);
         expect(result).toBe(expected);
     });
 });
+
+describe('Given arrayPush function', () => {
+    test(`The following array should be ${cases.push(extraCase)}`, () => {
+        const expected = cases.push(extraCase);
+        const result = cases.arrayPush(extraCase);
+        expect(result).toBe(expected);
+    });
+});
+describe('Given arrayPop function', () => {
+    test(`The following array should be ${cases.pop()}`, () => {
+        const expected = cases.length;
+        const result = arrayLength(cases);
+        expect(result).toBe(expected);
+    });
+});
+
